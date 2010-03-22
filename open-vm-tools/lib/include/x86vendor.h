44704ee73e677aad1cb2da697bf0d9a9810e1d8c
VMware, Inc <>
2010-03-22 15:12:39 -0700

    Fix tools build
    Signed-off-by: Marcelo Vanzin <mvanzin@vmware.com>

---
+++ open-vm-tools/lib/include/x86vendor.h
@@ -0,0 +1,51 @@
+
+/*********************************************************
+ * Copyright (C) 1998-2008 VMware, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation version 2.1 and no later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *********************************************************/
+
+/*********************************************************
+ * The contents of this file are subject to the terms of the Common
+ * Development and Distribution License (the "License") version 1.0
+ * and no later version.  You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ *         http://www.opensource.org/licenses/cddl1.php
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ *********************************************************/
+
+#ifndef _X86VENDOR_H_
+#define _X86VENDOR_H_
+
+/*
+ * CPU vendors
+ */
+
+typedef enum {
+   CPUID_VENDOR_UNKNOWN,
+   CPUID_VENDOR_COMMON,
+   CPUID_VENDOR_INTEL,
+   CPUID_VENDOR_AMD,
+   CPUID_VENDOR_CYRIX,
+   CPUID_VENDOR_VIA,
+   CPUID_NUM_VENDORS
+} CpuidVendor;
+
+#endif
