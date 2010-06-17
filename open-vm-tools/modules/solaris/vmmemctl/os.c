10bc1a491ee21a2c224feb40e004ba2bbedd7795
VMware, Inc <>
2010-06-17 15:11:38 -0700

    vmmemctl: do not require userspace component on Solaris
    Instead of using userspace program to provide a separate thread on
    Solaris create a proper kernel thread.
    
    This removes vmmemctld and the need to start/stop and muck around
    with it.
    
    Signed-off-by: Marcelo Vanzin <mvanzin@vmware.com>

--- open-vm-tools/modules/solaris/vmmemctl/os.c
+++ open-vm-tools/modules/solaris/vmmemctl/os.c
@@ -33,7 +33,7 @@
 #include <sys/kstat.h>
 #include <sys/id_space.h>
 #include <sys/vnode.h>
-#include <sys/taskq.h>
+#include <sys/proc.h>
 #include <sys/disp.h>
 #include <sys/ksynch.h>
 
@@ -41,7 +41,6 @@
 #include "vmballoon.h"
 #include "balloon_def.h"
 #include "vmballoon_kstats.h"
-#include "vmmemctl.h"
 #include "buildNumber.h"
 
 #if defined(SOL9)
@@ -62,6 +61,9 @@ extern void memscrub_disable(void);
 
 typedef struct {
    timeout_id_t id;
+
+   /* Worker thread ID */
+   kt_did_t thread_id;
 
    /* termination flag */
    volatile int stop;
@@ -98,7 +100,6 @@ typedef struct {
  */
 
 static os_state global_state;
-static dev_info_t *vmmemctl_dip;	/* only one instance */
 
 
 /*
@@ -421,34 +422,33 @@ OS_ReservedPageFree(PageHandle handle) // IN: A valid page handle
  *-----------------------------------------------------------------------------
  */
 
-static int
-os_worker(void)
+static void
+os_worker(os_timer *t)	// IN
 {
-   os_timer *t = &global_state.timer;
    clock_t timeout;
 
    mutex_enter(&t->lock);
-   while (!t->stop) {
-      /* invoke registered handler */
-      mutex_exit(&t->lock);
-      (void) (*(t->handler))(t->data);
-      mutex_enter(&t->lock);
 
+   while (!t->stop) {
+      mutex_exit(&t->lock);
+
+      /* invoke registered handler */
+      (void) (*(t->handler))(t->data);
+
+      mutex_enter(&t->lock);
       /* check again whether we should stop */
       if (t->stop)
-	 break;
+         break;
 
       /* wait for timeout */
       (void) drv_getparm(LBOLT, &timeout);
       timeout += t->period;
-      if (cv_timedwait_sig(&t->cv, &t->lock, timeout) == 0) {
-	 mutex_exit(&t->lock);
-	 return EINTR;		/* took a signal, return to user level */
-      }
+      cv_timedwait_sig(&t->cv, &t->lock, timeout);
    }
+
    mutex_exit(&t->lock);
-   ASSERT(t->stop);
-   return 0;			/* normal termination */
+
+   thread_exit();
 }
 
 
@@ -473,6 +473,7 @@ OS_TimerStart(OSTimerHandler *handler, // IN
               void *clientData)        // IN
 {
    os_timer *t = &global_state.timer;
+   kthread_t *tp;
 
    /* setup the timer structure */
    t->id = 0;
@@ -485,6 +486,13 @@ OS_TimerStart(OSTimerHandler *handler, // IN
 
    /* start the timer */
    t->stop = 0;
+
+   /*
+    * All Solaris drivers that I checked assume that thread_create() will
+    * succeed, let's follow the suit.
+    */
+   tp = thread_create(NULL, 0, os_worker, (void *)t, 0, &p0, TS_RUN, minclsyspri);
+   t->thread_id = tp->t_did;
 
    return TRUE;
 }
@@ -513,13 +521,19 @@ OS_TimerStop(void)
 
    mutex_enter(&t->lock);
 
-   /* set termination flag */
+   /* Set termination flag. */
    t->stop = 1;
 
-   /* wake up worker thread so it can exit */
+   /* Wake up worker thread so it can exit. */
    cv_signal(&t->cv);
 
    mutex_exit(&t->lock);
+
+   /* Wait for the worker thread to complete. */
+   if (t->thread_id != 0) {
+      thread_join(t->thread_id);
+      t->thread_id = 0;
+   }
 }
 
 
@@ -625,152 +639,17 @@ OS_Cleanup(void)
 
 
 /*
- * Device configuration entry points
- */
-
-
-static int
-vmmemctl_attach(dev_info_t *dip,      // IN
-                ddi_attach_cmd_t cmd) // IN
-{
-   switch (cmd) {
-   case DDI_ATTACH:
-      vmmemctl_dip = dip;
-      if (ddi_create_minor_node(dip, "0", S_IFCHR, ddi_get_instance(dip),
-				DDI_PSEUDO,0) != DDI_SUCCESS) {
-	 return DDI_FAILURE;
-      } else {
-	 return DDI_SUCCESS;
-      }
-   default:
-      return DDI_FAILURE;
-   }
-}
-
-
-static int
-vmmemctl_detach(dev_info_t *dip,      // IN
-                ddi_detach_cmd_t cmd) // IN
-{
-   switch (cmd) {
-   case DDI_DETACH:
-      vmmemctl_dip = 0;
-      ddi_remove_minor_node(dip, NULL);
-      return DDI_SUCCESS;
-   default:
-      return DDI_FAILURE;
-   }
-}
-
-
-/*
- *-----------------------------------------------------------------------------
- *
- * vmmemctl_ioctl --
- *
- *      Commands used by the user level daemon to control the driver.
- *      Since the daemon is single threaded, we use a simple monitor to
- *      make sure that only one thread is executing here at a time.
- *
- * Results:
- *      On success: 0
- *      On failure: error code
- *
- * Side effects:
- *      None
- *
- *-----------------------------------------------------------------------------
- */
-
-static int
-vmmemctl_ioctl(dev_t dev,    // IN: Unused
-               int cmd,      // IN
-               intptr_t arg, // IN: Unused
-               int mode,     // IN: Unused
-               cred_t *cred, // IN
-               int *rvalp)   // IN: Unused
-{
-   int error = 0;
-   static int busy = 0;		/* set when a thread is in this function */
-   static kmutex_t lock;	/* lock to protect busy count */
-
-   if (drv_priv(cred) != 0)
-      return EPERM;
-
-   mutex_enter(&lock);
-   if (busy) {
-      /*
-       * Only one thread at a time.
-       */
-      mutex_exit(&lock);
-      return EBUSY;
-   }
-   busy = 1;
-   mutex_exit(&lock);
-
-   switch (cmd) {
-   case VMMIOCWORK:
-      error = os_worker();
-      break;
-
-   default:
-      error = ENXIO;
-      break;
-   }
-
-   mutex_enter(&lock);
-   ASSERT(busy);
-   busy = 0;
-   mutex_exit(&lock);
-
-   return error;
-}
-
-/*
  * Module linkage
  */
 
-static struct cb_ops vmmemctl_cb_ops = {
-   nulldev,		/* open */
-   nulldev,		/* close */
-   nodev,		/* strategy */
-   nodev,		/* print */
-   nodev,		/* dump */
-   nodev,		/* read */
-   nodev,		/* write */
-   vmmemctl_ioctl,
-   nodev,		/* devmap */
-   nodev,		/* mmap */
-   nodev,		/* segmap */
-   nochpoll,		/* poll */
-   ddi_prop_op,		/* prop_op */
-   0,			/* streamtab */
-   D_NEW | D_MP
-};
-
-static struct dev_ops vmmemctl_dev_ops = {
-   DEVO_REV,
-   0,
-   ddi_no_info,		/* getinfo */
-   nulldev,		/* identify */
-   nulldev,		/* probe */
-   vmmemctl_attach,
-   vmmemctl_detach,
-   nodev,		/* reset */
-   &vmmemctl_cb_ops,	/* cb_ops */
-   NULL,		/* bus_ops */
-   nodev		/* power */
-};
-
 static struct modldrv vmmodldrv = {
-   &mod_driverops,
+   &mod_miscops,
    "VMware Memory Control b" BUILD_NUMBER_NUMERIC_STRING,
-   &vmmemctl_dev_ops
 };
 
 static struct modlinkage vmmodlinkage = {
    MODREV_1,
-   {&vmmodldrv, NULL}
+   { &vmmodldrv, NULL }
 };
 
 
