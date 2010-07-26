a632d6dbf7281f7b4ecd277378fbc9bca64e8f71
VMware, Inc <>
2010-07-26 12:22:56 -0700

    Changes in shared code that don't affect open-vm-tools functionality.
    Signed-off-by: Marcelo Vanzin <mvanzin@vmware.com>

--- open-vm-tools/lib/include/vm_version.h
+++ open-vm-tools/lib/include/vm_version.h
@@ -266,7 +266,7 @@
  * major version should be bumped with every major hosted release (and the minor to 0)
  * while the minor version should be bumped with each change affecting the USB Arbitrator.
  */
-#define USB_ARBITRATOR_COMPONENT_VERSION_NUMBER "2.3"
+#define USB_ARBITRATOR_COMPONENT_VERSION_NUMBER "2.4"
 
 #ifdef VMX86_VPX
 #define VIM_API_TYPE "VirtualCenter"
