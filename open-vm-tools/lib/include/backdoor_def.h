b910e4c74503efe86a066efcc48437f97cf0c94c
VMware, Inc <>
2010-11-18 14:16:06 -0800

    Bring comments in backdoor_def.h up to date.
    Some of the backdoor commands have been commented in backdoor_def.h
    indicating if they are devel only or if they are now unused.  I went
    through the whole list and udpated the comments to match the current
    code.  (I also lined up the numbers.)  No functional change.
    
    Signed-off-by: Marcelo Vanzin <mvanzin@vmware.com>

--- open-vm-tools/lib/include/backdoor_def.h
+++ open-vm-tools/lib/include/backdoor_def.h
@@ -60,7 +60,7 @@
 
 #define BDOOR_PORT 0x5658
 
-#define BDOOR_CMD_GETMHZ      		   1
+#define   BDOOR_CMD_GETMHZ      		    1
 /*
  * BDOOR_CMD_APMFUNCTION is used by:
  *
@@ -73,87 +73,86 @@
  *
  *  --hpreg
  */
-#define BDOOR_CMD_APMFUNCTION 		   2
-#define BDOOR_CMD_GETDISKGEO  		   3
-#define BDOOR_CMD_GETPTRLOCATION	      4
-#define BDOOR_CMD_SETPTRLOCATION	      5
-#define BDOOR_CMD_GETSELLENGTH		   6
-#define BDOOR_CMD_GETNEXTPIECE		   7
-#define BDOOR_CMD_SETSELLENGTH		   8
-#define BDOOR_CMD_SETNEXTPIECE		   9
-#define BDOOR_CMD_GETVERSION		      10
-#define BDOOR_CMD_GETDEVICELISTELEMENT	11
-#define BDOOR_CMD_TOGGLEDEVICE		   12
-#define BDOOR_CMD_GETGUIOPTIONS		   13
-#define BDOOR_CMD_SETGUIOPTIONS		   14
-#define BDOOR_CMD_GETSCREENSIZE		   15
-#define BDOOR_CMD_MONITOR_CONTROL       16
-#define BDOOR_CMD_GETHWVERSION          17
-#define BDOOR_CMD_OSNOTFOUND            18
-#define BDOOR_CMD_GETUUID               19
-#define BDOOR_CMD_GETMEMSIZE            20
-#define BDOOR_CMD_HOSTCOPY              21 /* Devel only */
-#define BDOOR_CMD_SERVICE_VM            22 /* Unused, never shipped, prototype only */         
-#define BDOOR_CMD_GETTIME               23 /* Deprecated. Use GETTIMEFULL. */
-#define BDOOR_CMD_STOPCATCHUP           24
-#define BDOOR_CMD_PUTCHR	        25 /* Devel only */
-#define BDOOR_CMD_ENABLE_MSG	        26 /* Devel only */
-#define BDOOR_CMD_GOTO_TCL	        27 /* Devel only */
-#define BDOOR_CMD_INITPCIOPROM		28
-#define BDOOR_CMD_INT13			29
-#define BDOOR_CMD_MESSAGE               30
-#define BDOOR_CMD_SIDT                  31
-#define BDOOR_CMD_SGDT                  32
-#define BDOOR_CMD_SLDT_STR              33
-#define BDOOR_CMD_ISACPIDISABLED	34
-#define BDOOR_CMD_TOE			35 /* Not in use */
-#define BDOOR_CMD_ISMOUSEABSOLUTE       36
-#define BDOOR_CMD_PATCH_SMBIOS_STRUCTS  37
-#define BDOOR_CMD_MAPMEM                38 /* Devel only */
-#define BDOOR_CMD_ABSPOINTER_DATA	39
-#define BDOOR_CMD_ABSPOINTER_STATUS	40
-#define BDOOR_CMD_ABSPOINTER_COMMAND	41
-#define BDOOR_CMD_TIMER_SPONGE          42 /* Not supported anymore */
-#define BDOOR_CMD_PATCH_ACPI_TABLES	43
-/* Catch-all to allow synchronous tests */
-#define BDOOR_CMD_DEVEL_FAKEHARDWARE	44 /* Debug only - needed in beta */
-#define BDOOR_CMD_GETHZ      		45
-#define BDOOR_CMD_GETTIMEFULL           46
-#define BDOOR_CMD_STATELOGGER           47
-#define BDOOR_CMD_CHECKFORCEBIOSSETUP	48
-#define BDOOR_CMD_LAZYTIMEREMULATION    49
-#define BDOOR_CMD_BIOSBBS               50
-#define BDOOR_CMD_VASSERT               51
-#define BDOOR_CMD_ISGOSDARWIN           52
-#define BDOOR_CMD_DEBUGEVENT            53
-#define BDOOR_CMD_OSNOTMACOSXSERVER     54
-#define BDOOR_CMD_GETTIMEFULL_WITH_LAG  55
-#define BDOOR_CMD_ACPI_HOTPLUG_DEVICE   56
-#define BDOOR_CMD_ACPI_HOTPLUG_MEMORY   57
-#define BDOOR_CMD_ACPI_HOTPLUG_CBRET    58
-#define BDOOR_CMD_GET_HOST_VIDEO_MODES  59 /* Not in use */
-#define BDOOR_CMD_ACPI_HOTPLUG_CPU      60
-#define BDOOR_CMD_USB_HOTPLUG_MOUSE     61 /* Not in use */
-#define BDOOR_CMD_XPMODE                62
-#define BDOOR_CMD_NESTING_CONTROL       63
-#define BDOOR_CMD_FIRMWARE_INIT         64
-#define BDOOR_CMD_FIRMWARE_ACPI_SERVICES 65
-#  define BDOOR_CMD_FAS_GET_TABLE_SIZE    0
-#  define BDOOR_CMD_FAS_GET_TABLE_DATA    1
-#  define BDOOR_CMD_FAS_GET_PLATFORM_NAME 2
-#define BDOOR_CMD_SENDPSHAREHINTS       66
-#define BDOOR_CMD_ENABLE_USB_MOUSE      67
-#define BDOOR_CMD_GET_VCPU_INFO         68
-#  define BDOOR_CMD_VCPU_SLC64            0
-#  define BDOOR_CMD_VCPU_SYNC_VTSCS       1
-#  define BDOOR_CMD_VCPU_HV_REPLAY_OK     2
-#  define BDOOR_CMD_VCPU_RESERVED        31
-#define BDOOR_CMD_EFI_SERIALCON_CONFIG   69
-#define BDOOR_CMD_BUG328986              70
-#define BDOOR_CMD_FIRMWARE_ERROR         71
-#  define BDOOR_CMD_FE_INSUFFICIENT_MEM   0
-#  define BDOOR_CMD_FE_EXCEPTION          1
-#define BDOOR_CMD_MAX                    72
+#define   BDOOR_CMD_APMFUNCTION               2 /* CPL0 only. */
+#define   BDOOR_CMD_GETDISKGEO                3
+#define   BDOOR_CMD_GETPTRLOCATION            4
+#define   BDOOR_CMD_SETPTRLOCATION            5
+#define   BDOOR_CMD_GETSELLENGTH              6
+#define   BDOOR_CMD_GETNEXTPIECE              7
+#define   BDOOR_CMD_SETSELLENGTH              8
+#define   BDOOR_CMD_SETNEXTPIECE              9
+#define   BDOOR_CMD_GETVERSION               10
+#define   BDOOR_CMD_GETDEVICELISTELEMENT     11
+#define   BDOOR_CMD_TOGGLEDEVICE             12
+#define   BDOOR_CMD_GETGUIOPTIONS            13
+#define   BDOOR_CMD_SETGUIOPTIONS            14
+#define   BDOOR_CMD_GETSCREENSIZE            15
+#define   BDOOR_CMD_MONITOR_CONTROL          16 /* Disabled by default. */
+#define   BDOOR_CMD_GETHWVERSION             17
+#define   BDOOR_CMD_OSNOTFOUND               18 /* CPL0 only. */
+#define   BDOOR_CMD_GETUUID                  19
+#define   BDOOR_CMD_GETMEMSIZE               20
+#define   BDOOR_CMD_HOSTCOPY                 21 /* Devel only. */
+//#define BDOOR_CMD_SERVICE_VM               22 /* Not in use. Never shipped. */
+#define   BDOOR_CMD_GETTIME                  23 /* Deprecated -> GETTIMEFULL. */
+#define   BDOOR_CMD_STOPCATCHUP              24
+#define   BDOOR_CMD_PUTCHR                   25 /* Disabled by default. */
+#define   BDOOR_CMD_ENABLE_MSG               26 /* Devel only. */
+#define   BDOOR_CMD_GOTO_TCL                 27 /* Devel only. */
+#define   BDOOR_CMD_INITPCIOPROM             28 /* CPL 0 only. */
+//#define BDOOR_CMD_INT13                    29 /* Not in use. */
+#define   BDOOR_CMD_MESSAGE                  30
+#define   BDOOR_CMD_SIDT                     31
+#define   BDOOR_CMD_SGDT                     32
+#define   BDOOR_CMD_SLDT_STR                 33
+#define   BDOOR_CMD_ISACPIDISABLED           34
+//#define BDOOR_CMD_TOE                      35 /* Not in use. */
+#define   BDOOR_CMD_ISMOUSEABSOLUTE          36
+#define   BDOOR_CMD_PATCH_SMBIOS_STRUCTS     37 /* CPL 0 only. */
+#define   BDOOR_CMD_MAPMEM                   38 /* Devel only */
+#define   BDOOR_CMD_ABSPOINTER_DATA          39
+#define   BDOOR_CMD_ABSPOINTER_STATUS        40
+#define   BDOOR_CMD_ABSPOINTER_COMMAND       41
+//#define BDOOR_CMD_TIMER_SPONGE             42 /* Not in use. */
+#define   BDOOR_CMD_PATCH_ACPI_TABLES        43 /* CPL 0 only. */
+//#define BDOOR_CMD_DEVEL_FAKEHARDWARE       44 /* Not in use. */
+#define   BDOOR_CMD_GETHZ                    45
+#define   BDOOR_CMD_GETTIMEFULL              46
+#define   BDOOR_CMD_STATELOGGER              47 /* Disabled by default. */
+#define   BDOOR_CMD_CHECKFORCEBIOSSETUP      48 /* CPL 0 only. */
+#define   BDOOR_CMD_LAZYTIMEREMULATION       49 /* CPL 0 only. */
+#define   BDOOR_CMD_BIOSBBS                  50 /* CPL 0 only. */
+//#define BDOOR_CMD_VASSERT                  51 /* Not in use. */
+#define   BDOOR_CMD_ISGOSDARWIN              52
+#define   BDOOR_CMD_DEBUGEVENT               53
+#define   BDOOR_CMD_OSNOTMACOSXSERVER        54 /* CPL 0 only. */
+#define   BDOOR_CMD_GETTIMEFULL_WITH_LAG     55
+#define   BDOOR_CMD_ACPI_HOTPLUG_DEVICE      56 /* Devel only. */
+#define   BDOOR_CMD_ACPI_HOTPLUG_MEMORY      57 /* Devel only. */
+#define   BDOOR_CMD_ACPI_HOTPLUG_CBRET       58 /* Devel only. */
+//#define BDOOR_CMD_GET_HOST_VIDEO_MODES     59 /* Not in use. */
+#define   BDOOR_CMD_ACPI_HOTPLUG_CPU         60 /* Devel only. */
+//#define BDOOR_CMD_USB_HOTPLUG_MOUSE        61 /* Not in use. Never shipped. */
+#define   BDOOR_CMD_XPMODE                   62 /* CPL 0 only. */
+#define   BDOOR_CMD_NESTING_CONTROL          63
+#define   BDOOR_CMD_FIRMWARE_INIT            64 /* CPL 0 only. */
+#define   BDOOR_CMD_FIRMWARE_ACPI_SERVICES   65 /* CPL 0 only. */
+#  define BDOOR_CMD_FAS_GET_TABLE_SIZE        0
+#  define BDOOR_CMD_FAS_GET_TABLE_DATA        1
+#  define BDOOR_CMD_FAS_GET_PLATFORM_NAME     2
+#define   BDOOR_CMD_SENDPSHAREHINTS          66
+#define   BDOOR_CMD_ENABLE_USB_MOUSE         67
+#define   BDOOR_CMD_GET_VCPU_INFO            68
+#  define BDOOR_CMD_VCPU_SLC64                0
+#  define BDOOR_CMD_VCPU_SYNC_VTSCS           1
+#  define BDOOR_CMD_VCPU_HV_REPLAY_OK         2
+#  define BDOOR_CMD_VCPU_RESERVED            31
+#define   BDOOR_CMD_EFI_SERIALCON_CONFIG     69 /* CPL 0 only. */
+#define   BDOOR_CMD_BUG328986                70 /* CPL 0 only. */
+#define   BDOOR_CMD_FIRMWARE_ERROR           71 /* CPL 0 only. */
+#  define BDOOR_CMD_FE_INSUFFICIENT_MEM       0
+#  define BDOOR_CMD_FE_EXCEPTION              1
+#define   BDOOR_CMD_MAX                      72
 
 
 /* 
