b98a035a0d663a07c988a8de664e571d09d23427
VMware, Inc <>
2011-05-28 12:53:54 -0700

    Static analysis bug: NO_EFFECT
    A trivial fix.
    
    Signed-off-by: Marcelo Vanzin <mvanzin@vmware.com>

--- open-vm-tools/services/plugins/dndcp/dnd/dndLinux.c
+++ open-vm-tools/services/plugins/dndcp/dnd/dndLinux.c
@@ -493,7 +493,7 @@ static Bool
 DnD_CheckBlockFuse(int blockFd)                    // IN
 {
    char buf[sizeof(VMBLOCK_FUSE_READ_RESPONSE)];
-   size_t size;
+   ssize_t size;
 
    size = read(blockFd, buf, sizeof(VMBLOCK_FUSE_READ_RESPONSE));
    if (size < 0) {
