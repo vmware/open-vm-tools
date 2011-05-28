80901d02a1dfab943a3a404eae575f5a14b56afd
VMware, Inc <>
2011-05-28 12:56:50 -0700

    shutdownDgramTest fails  for ubuntu9 guest
    We fixed shutdown() recently so that it kicks DGRAM sockets
    even if they are unconnected.  But to be truly consistent
    with UDP, we should still return an error if they are
    unconnected.  Fixed so that we kick the socket and
    then fail.
    
    Signed-off-by: Marcelo Vanzin <mvanzin@vmware.com>

--- open-vm-tools/modules/linux/vsock/linux/af_vsock.c
+++ open-vm-tools/modules/linux/vsock/linux/af_vsock.c
@@ -4118,6 +4118,7 @@ static int
 VSockVmciShutdown(struct socket *sock,  // IN
                   int mode)             // IN
 {
+   int32 err;
    struct sock *sk;
 
    /*
@@ -4133,12 +4134,23 @@ VSockVmciShutdown(struct socket *sock,  // IN
       return -EINVAL;
    }
 
-   sk = sock->sk;
-   if (sk->sk_type == SOCK_STREAM && sock->state == SS_UNCONNECTED) {
-      return -ENOTCONN;
-   }
+   /*
+    * If this is a STREAM socket and it is not connected then bail out
+    * immediately.  If it is a DGRAM socket then we must first kick the socket
+    * so that it wakes up from any sleeping calls, for example recv(), and then
+    * afterwards return the error.
+    */
 
-   sock->state = SS_DISCONNECTING;
+   sk = sock->sk;
+   if (sock->state == SS_UNCONNECTED) {
+      err = -ENOTCONN;
+      if (sk->sk_type == SOCK_STREAM) {
+         return err;
+      }
+   } else {
+      sock->state = SS_DISCONNECTING;
+      err = 0;
+   }
 
    /* Receive and send shutdowns are treated alike. */
    mode = mode & (RCV_SHUTDOWN | SEND_SHUTDOWN);
@@ -4154,7 +4166,7 @@ VSockVmciShutdown(struct socket *sock,  // IN
       VSOCK_SEND_SHUTDOWN(sk, mode);
    }
 
-   return 0;
+   return err;
 }
 
 
