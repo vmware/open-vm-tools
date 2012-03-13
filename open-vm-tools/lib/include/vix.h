b383087dfdfe03ee60e4bf3cf533366727415ad8
VMware, Inc <>
2012-03-13 13:04:05 -0700

    Return an error if the specified run script text is too large.
    VixMsg_AllocRequestMsg() ASSERTs if the message size exceeds
    VIX_COMMAND_MAX_REQUEST_SIZE. Made code changes to return an error
    if the specified script text is too large.
    
    Defined a new error code for this specific error case.
    Added a remark in the documentation for VixVM_RunScriptInGuest() function.
    
    Signed-off-by: Dmitry Torokhov <dtor@vmware.com>

--- open-vm-tools/lib/include/vix.h
+++ open-vm-tools/lib/include/vix.h
@@ -120,6 +120,7 @@ enum {
    VIX_E_AUTHENTICATION_FAIL                    = 35,
    VIX_E_HOST_CONNECTION_LOST                   = 36,
    VIX_E_DUPLICATE_NAME                         = 41,
+   VIX_E_ARGUMENT_TOO_BIG                       = 44,
 
    /* Handle Errors */
    VIX_E_INVALID_HANDLE                         = 1000,
