90766520c9a335c6ac1baaa5845b8dfb3f559339
Dmitry Torokhov <dtor@vmware.com>
2012-05-21 16:30:01 -0700

    Updating open-vm-tools version for release 2012.05.21-724730.
    Signed-off-by: Dmitry Torokhov <dtor@vmware.com>

--- open-vm-tools/modules/linux/dkms.sh
+++ open-vm-tools/modules/linux/dkms.sh
@@ -28,12 +28,12 @@ then
    echo "   src:  root of unpacked open-vm-tools package"
    echo "   dst:  where to create the dkms tree"
    echo
-   echo "The script will create an 'open-vm-tools' module with version 2012.03.13."
+   echo "The script will create an 'open-vm-tools' module with version 2012.05.21."
    exit 1
 fi
 
 src=$1
-dst=$2/open-vm-tools-2012.03.13
+dst=$2/open-vm-tools-2012.05.21
 
 SHARED_HEADERS="backdoor_def.h"
 SHARED_HEADERS="$SHARED_HEADERS backdoor_types.h"
