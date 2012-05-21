2675d8c8c870b09da4986e67d04e429c06052732
VMware, Inc <>
2012-05-21 15:23:16 -0700

    Add support for processor groups to Windows vmmon
    Many OS APIs represent processors using a uintptr_t bitfield.
    Processor groups were added in Win7/W2K8R2 to get beyond the limit
    of 64-bit processors on x64.  Currently at most 4 groups, or 256
    processors, are supported.  This happens to match the current
    max pcpus define.
    
    This change utilizes the Win7 WDK and procgrp.h, which uses
    some #defines and thunking to use the native newer APIs on
    Win7-and-later, while emulating the behavior on older OSes using
    older existing APIs.
    
    By default a process is limited to one processor group.  Certain
    operations, e.g., get/set TSCs will only be performed across the
    processor group that the vmx process is running on.
    
    HostIF_CallOnEachCPU() will eventually need to be modified to run
    across all processor groups rather than the current processor group.
    The vmx still needs to be modified to do a more elaborate determination
    of the # of processors because the standard OS APIs only return the
    vmx passing down a buffer that's too small for IOCTL_VMX86_GET_ALL_MSRS.
    This function is also used as part of Task_Terminate()'s call to
    TaskIsVMXDisabledOnAllCPUs(), and the call to Vmx86_EnableHV that's
    triggered by IOCTL_VMX86_ENABLE_HV.
    
    I also removed some logs that spew too much when running a VM
    with a kernel debugger attached.
    
    bcdedit.exe /set groupaware on
    
    Signed-off-by: Dmitry Torokhov <dtor@vmware.com>

--- open-vm-tools/lib/include/vm_basic_defs.h
+++ open-vm-tools/lib/include/vm_basic_defs.h
@@ -688,7 +688,11 @@ typedef int pid_t;
 #ifdef _WIN32
 #ifndef USES_OLD_WINDDK
 #if defined(VMX86_LOG)
+#ifdef _WIN64
+#define WinDrvPrint(arg, ...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, ~0, arg, __VA_ARGS__)
+#else
 #define WinDrvPrint(arg, ...) DbgPrint(arg, __VA_ARGS__)
+#endif
 #define WinDrvEngPrint(arg, ...) EngDbgPrint(arg, __VA_ARGS__)
 #else
 #define WinDrvPrint(arg, ...)
