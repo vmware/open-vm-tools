aad82517fe6a5442c4b90b6e99d0c53cdbe81fce
VMware, Inc <>
2013-04-12 12:42:23 -0700

    One copy of lib/err globals
    It looks like we have multiple copies of errNumTable but only one
    errStrTable. Each new copy of errNumTable comes for each library that
    includes lib/err. Since errStrTable is not declared static, only one
    exists between all shared libraries.
    
    This change makes both errNumTable and errPtrTable non-static as
    well. The intention is that we'll have only one copy of the errno
    accounting across the entire binary.
    
    Generally sharing globals across a bunch of libraries sounds
    dangerous, but in this case it's an atomic hashtable so it works.
    
    Signed-off-by: Dmitry Torokhov <dtor@vmware.com>

--- open-vm-tools/lib/err/err.c
+++ open-vm-tools/lib/err/err.c
@@ -53,18 +53,25 @@ typedef struct ErrInfo {
  * Variables
  */
 
-static Atomic_Ptr errNumTable;
-static Atomic_Ptr errPtrTable;
-
-#define NUMTABLE() HashTable_AllocOnce(&errNumTable, HASHTABLE_SIZE, \
-				       HASH_INT_KEY | HASH_FLAG_ATOMIC, NULL)
-#define PTRTABLE() HashTable_AllocOnce(&errPtrTable, HASHTABLE_SIZE, \
-				       HASH_INT_KEY | HASH_FLAG_ATOMIC, NULL)
+/*
+ * We statically link lib/err in several libraries. This means that a
+ * single binary may have several copies of lib/err. These pointers are
+ * not static so that we have one copy across the entire binary.
+ */
+Atomic_Ptr errNumTable;
+Atomic_Ptr errPtrTable;
 #if defined VMX86_DEBUG && defined __linux__
 Atomic_Ptr errStrTable;
+#endif
+
+#define NUMTABLE() HashTable_AllocOnce(&errNumTable, HASHTABLE_SIZE, \
+                                       HASH_INT_KEY | HASH_FLAG_ATOMIC, NULL)
+#define PTRTABLE() HashTable_AllocOnce(&errPtrTable, HASHTABLE_SIZE, \
+                                       HASH_INT_KEY | HASH_FLAG_ATOMIC, NULL)
+#if defined VMX86_DEBUG && defined __linux__
 #define STRTABLE() HashTable_AllocOnce(&errStrTable, HASHTABLE_SIZE, \
-				       HASH_STRING_KEY | HASH_FLAG_ATOMIC, \
-				       NULL)
+                                       HASH_STRING_KEY | HASH_FLAG_ATOMIC, \
+                                       NULL)
 #endif
 
 
