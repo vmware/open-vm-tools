d58086d3f3218024ecee479034f4441a27fe278f
Oliver Kurth <okurth@vmware.com>
2017-07-28 12:16:51 -0700

    The previous change to the Linux VMCI driver resulted in intermittent hangs during soft reboots of RHEL 6.8 Linux VM.  This change reverts that code change and resets the driver version number.
    This reverts commit 7023998ba5594871a658c5ef1be4dbda6c0158eb.

--- open-vm-tools/modules/linux/vmci/linux/driver.c
+++ open-vm-tools/modules/linux/vmci/linux/driver.c
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2011-2014,2017 VMware, Inc. All rights reserved.
+ * Copyright (C) 2011-2014 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -2015,8 +2015,6 @@ vmci_remove_device(struct pci_dev* pdev)
    dev->exclusive_vectors = FALSE;
    dev->intr_type = VMCI_INTR_TYPE_INTX;
 
-   tasklet_disable(&vmci_dg_tasklet);
-   tasklet_disable(&vmci_bm_tasklet);
    tasklet_kill(&vmci_dg_tasklet);
    tasklet_kill(&vmci_bm_tasklet);
 
@@ -2478,7 +2476,6 @@ vmci_exit(void)
    if (guestDeviceInit) {
       pci_unregister_driver(&vmci_driver);
       vfree(data_buffer);
-      data_buffer = NULL;
       guestDeviceInit = FALSE;
    }
 
