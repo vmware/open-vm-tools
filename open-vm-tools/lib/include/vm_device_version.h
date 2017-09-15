27d72451d479d96f1d1de901cb76820f5bdac38e
Oliver Kurth <okurth@vmware.com>
2017-09-15 11:23:56 -0700

    Change to shared header file unrelated to open-vm-tools.

--- open-vm-tools/lib/include/vm_device_version.h
+++ open-vm-tools/lib/include/vm_device_version.h
@@ -163,16 +163,13 @@
 #define PCI_DEVICE_ID_INTEL_LYNXPOINT_XHCI 0x8c31
 
 /*
- * Intel Quickassist (QAT) devices:
- * - Gen 1: Cave Creek (CVC) and Coleto Creek (CLC)
- * - Gen 2: Lewisburg (LBG)
+ * Intel Quickassist (QAT) devices.
  */
-#define PCI_DEVICE_ID_INTEL_QAT_CVC     0x0434
-#define PCI_DEVICE_ID_INTEL_QAT_CLC     0x0435
-#define PCI_DEVICE_ID_INTEL_QAT_LBG     0x37c8
-#define PCI_DEVICE_ID_INTEL_QAT_CVC_VF  0x0442
-#define PCI_DEVICE_ID_INTEL_QAT_CLC_VF  0x0443
-#define PCI_DEVICE_ID_INTEL_QAT_LBG_VF  0x37c9
+#define PCI_DEVICE_ID_INTEL_QAT_DH895XCC     0x0435
+#define PCI_DEVICE_ID_INTEL_QAT_DH895XCC_VF  0x0443
+
+#define PCI_DEVICE_ID_INTEL_QAT_C62X         0x37c8
+#define PCI_DEVICE_ID_INTEL_QAT_C62X_VF      0x37c9
 
 /*
  * Intel FPGAs
