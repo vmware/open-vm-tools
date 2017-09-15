04cf8f685846a7c817072f9fcfac6f6d9618b8c5
Oliver Kurth <okurth@vmware.com>
2017-09-15 11:23:37 -0700

    Downgrading 10.2.0 Tools to 10.0.9 fails to start CAF.
    Create the cafenv.config file required by 10.0.9 Tools
    from cafenv.appconfig of 10.2.0 Tools.
    
    Clean up CAF files after Tools uninstall.

--- open-vm-tools/common-agent/etc/install/preupgrade.sh
+++ open-vm-tools/common-agent/etc/install/preupgrade.sh
@@ -6,6 +6,8 @@ configDir=$installDir/../config
 #Preserve config
 mkdir -p "$configDir"/_previous_
 cp -pf "$configDir"/cafenv-appconfig "$configDir"/_previous_/ 2>/dev/null
+cp -pf "$configDir"/cafenv.config "$configDir"/_previous_/cafenv.config 2>/dev/null
+cat /tmp/_cafenv-appconfig_ >> "$configDir"/_previous_/cafenv.config
 
 #Temporary until we remove amqp_username/password
 cp -pf "$configDir"/CommAmqpListener-appconfig "$configDir"/_previous_/ 2>/dev/null
