013c3d530d203baa3ec02ad767d641cd495923a6
Oliver Kurth <okurth@vmware.com>
2017-09-15 11:23:11 -0700

    Revert previous commit.

--- open-vm-tools/lib/include/log.h
+++ open-vm-tools/lib/include/log.h
@@ -87,7 +87,6 @@ extern "C" {
  */
 
 #define VMW_LOG_LEVEL_MASK 0x000000FF  // Log level bits are in the LOB
-#define VMW_LOG_NO_BLOCK   0x80000000  // Message may be dropped
 
 void LogV(uint32 routing,
           const char *fmt,
