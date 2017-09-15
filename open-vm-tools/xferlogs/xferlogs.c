5dfef556fa4220a87c67177c3d3a82c1cd74bc5f
Oliver Kurth <okurth@vmware.com>
2017-09-15 11:23:21 -0700

    add -h/--help for xferlogs

--- open-vm-tools/xferlogs/xferlogs.c
+++ open-vm-tools/xferlogs/xferlogs.c
@@ -272,7 +272,10 @@ static void
 usage(void)
 {
    Warning("xferlogs <options> <filename>\n");
-   Warning("options - enc/dec\n");
+   Warning("options:\n");
+   Warning("\t-h | --help - prints this usage.\n");
+   Warning("\tenc - encodes and transfers <filename> to the VMX log.\n");
+   Warning("\tdec - extracts encoded data to <filename> from the VMX log.\n");
 }
 
 
@@ -281,6 +284,14 @@ main(int argc,
      char *argv[])
 {
    int status;
+
+   if (argc == 2 &&
+       (!strncmp(argv[1], "-h", 2) ||
+        !strncmp(argv[1], "--help", 6))) {
+      usage();
+      return 0;
+   }
+
    if (argc != 3) {
       usage();
       return -1;
@@ -288,9 +299,9 @@ main(int argc,
 
    if (!strncmp(argv[1], "enc", 3)) {
       xmitFile(argv[2]);
-   } else if(!strncmp(argv[1], "dec", 3)) {
+   } else if (!strncmp(argv[1], "dec", 3)) {
       extractFile(argv[2]);
-   } else if(!strncmp(argv[1], "upd", 3)) {
+   } else if (!strncmp(argv[1], "upd", 3)) {
       if (StrUtil_StrToInt(&status, argv[2])) {
          RpcOut_sendOne(NULL, NULL, RPC_VMSUPPORT_STATUS " %d", status);
       } else {
