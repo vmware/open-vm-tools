afff26f7e749a416483c59431f17f09d311bac2d
Oliver Kurth <okurth@vmware.com>
2017-09-15 11:23:20 -0700

    Change to shared header file unrelated to open-vm-tools.

--- open-vm-tools/lib/include/vm_product_versions.h
+++ open-vm-tools/lib/include/vm_product_versions.h
@@ -130,7 +130,7 @@
  * 4.0.0-1.8: patch 3
  */
 #define ESX_VERSION_MAJOR "6"
-#define ESX_VERSION_MINOR "5"
+#define ESX_VERSION_MINOR "6"
 #define ESX_VERSION_MAINT "0"
 #define ESX_VERSION ESX_VERSION_MAJOR "." ESX_VERSION_MINOR "." \
                     ESX_VERSION_MAINT
