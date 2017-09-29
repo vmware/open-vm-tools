8410d26cb69f48ad8de43704ca2bf0e020fc1b93
Oliver Kurth <okurth@vmware.com>
2017-09-29 12:52:44 -0700

    For FreeBSD 11 and later the correct osname is "freeBSD11". Without the correct osname, guestInfo ends up with wrong guestId and guestFullName.  Adjust the osname based on the FreeBSD release.

--- open-vm-tools/lib/include/guest_os.h
+++ open-vm-tools/lib/include/guest_os.h
@@ -493,7 +493,8 @@ Bool Gos_InSetArray(uint32 gos, const uint32 *set);
 #define STR_OS_WIN_64_BIT_EXTENSION ", 64-bit"
 
 /* FreeBSD */
-#define STR_OS_FREEBSD "freeBSD"
+#define STR_OS_FREEBSD   "freeBSD"
+#define STR_OS_FREEBSD11 "freeBSD11"
 
 /* Solaris */
 #define STR_OS_SOLARIS "solaris"
