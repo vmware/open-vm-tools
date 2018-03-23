cdf0406dc669f2284e1e810ac5820f123d46a50e
Oliver Kurth <okurth@vmware.com>
2018-03-23 15:05:35 -0700

    FreeBSD: Improper use of sysconf() for getpwent buffer size leads to          vmtoolsd crash.
    On FreeBSD, sysconf(_SC_GETPW_R_SIZE_MAX) can return -1 if it has
    no hard limit ultimately resulting in an incorrect buffer size.
    
    This change is adapting the sysconf() ifixes done elsewhere to
    bora-vmsoft/services/plugins/vix/vixTools.c and updating the
    open-vm-tools AUTHORS file to share credit for the pull request.
    
    https://github.com/vmware/open-vm-tools/pull/238

--- open-vm-tools/services/plugins/vix/vixTools.c
+++ open-vm-tools/services/plugins/vix/vixTools.c
@@ -10133,7 +10133,7 @@ abort:
    struct passwd pwd;
    struct passwd *ppwd = &pwd;
    char *buffer = NULL; // a pool of memory for Posix_Getpwnam_r() to use.
-   size_t bufferSize;
+   long bufferSize;
 
    /*
     * For POSIX systems, look up the uid of 'username', and compare
@@ -10146,9 +10146,15 @@ abort:
     * Multiply by 4 to compensate for the conversion to UTF-8 by
     * the Posix_Getpwnam_r() wrapper.
     */
-   bufferSize = (size_t) sysconf(_SC_GETPW_R_SIZE_MAX) * 4;
+   errno = 0;
+   bufferSize = sysconf(_SC_GETPW_R_SIZE_MAX);
+   if ((errno != 0) || (bufferSize <= 0)) {
+      bufferSize = 16 * 1024;  // Unlimited; pick something reasonable
+   }
 
-   buffer = Util_SafeMalloc(bufferSize);
+   bufferSize *= 4;
+
+   buffer = Util_SafeMalloc((size_t)bufferSize);
 
    if (Posix_Getpwnam_r(username, &pwd, buffer, bufferSize, &ppwd) != 0 ||
        NULL == ppwd) {
