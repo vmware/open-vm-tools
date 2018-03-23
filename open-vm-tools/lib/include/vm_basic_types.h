670dba44e90c64173a1f67e617a7193f18fc3a2c
Oliver Kurth <okurth@vmware.com>
2018-03-23 14:57:11 -0700

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_basic_types.h
+++ open-vm-tools/lib/include/vm_basic_types.h
@@ -642,6 +642,7 @@ typedef void * UserVA;
 #define MAX_PPN_BITS      31
 #define MAX_PPN           (((PPN)1 << MAX_PPN_BITS) - 1) /* Maximal observable PPN value. */
 #define INVALID_PPN       ((PPN)0xffffffff)
+#define INVALID_PPN64     ((PPN64)0xffffffffffffffffull)
 #define APIC_INVALID_PPN  ((PPN)0xfffffffe)
 
 #define INVALID_BPN       ((BPN)0x000000ffffffffffull)
