07bb14758597a16a2af7a1dd0be7c05ec7f946ad
Josh Paetzel <josh@tcbug.org>
2018-05-24 12:52:40 -0500

    Switch from STAILQ to CK_STAILQ in FreeBSD HEAD.
    CK_STAILQ is safe to traverse in the presence of
    conncurent deletion.

--- open-vm-tools/modules/freebsd/vmxnet/net_compat.h
+++ open-vm-tools/modules/freebsd/vmxnet/net_compat.h
@@ -37,9 +37,14 @@
 #if __FreeBSD_version < 500016
    #define VXN_IFMULTI_FIRST LIST_FIRST
    #define VXN_IFMULTI_NEXT  LIST_NEXT
-#else /* >= 500016 */
+#else
+#if __FreeBSD_version <= 1200063
    #define VXN_IFMULTI_FIRST TAILQ_FIRST
    #define VXN_IFMULTI_NEXT  TAILQ_NEXT
+#else /* > 1200063 */
+   #define VXN_IFMULTI_FIRST CK_STAILQ_FIRST
+   #define VXN_IFMULTI_NEXT  CK_STAILQ_NEXT
+#endif /* 1200063 */
 #endif /* 500016 */
 
 #if __FreeBSD_version < 500043
