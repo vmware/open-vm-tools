0a2d2f8baf9e4dccb2fd15c833efef5e704683ea
Oliver Kurth <okurth@vmware.com>
2018-07-13 15:20:38 -0700

    Workaround for false negative result when detecting cloud-init existance
    "cloud-init -v" cmd is used to detect if cloud-init is properly configured and
    it works on most linux distros. However in some linux distro like Amazon Linux 2,
    "cloud-init -v" will print result to stderr instead of stdout and it makes
    "forkExecAndWaitCommand" give false negative result.
    
    1. added a new bool switch in  "ForkExecAndWaitCommand" to choose
       if we should ignore the stderr output when the return code is 0
    2. removed unnecessary reference for "ForkExecAndWaitCommand" in linuxDeploymentUtilities.c
    3. trivial change for some formatting

--- open-vm-tools/libDeployPkg/linuxDeploymentUtilities.c
+++ open-vm-tools/libDeployPkg/linuxDeploymentUtilities.c
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2016-2017 VMware, Inc. All rights reserved.
+ * Copyright (C) 2016-2018 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -24,7 +24,6 @@
 #include <regex.h>
 #include "linuxDeploymentUtilities.h"
 
-extern int ForkExecAndWaitCommand(const char* command);
 extern LogFunction sLog;
 
 /**
