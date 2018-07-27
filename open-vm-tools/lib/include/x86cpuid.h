727871f9681b398330d6f186035bbb2c51369353
Oliver Kurth <okurth@vmware.com>
2018-07-27 11:46:19 -0700

    Common header file change; not applicable to open-vm-tools.

--- open-vm-tools/lib/include/x86cpuid.h
+++ open-vm-tools/lib/include/x86cpuid.h
@@ -664,7 +664,7 @@ FIELD( 10,  1, EDX,  0, 16, MAX_COS_NUMBER,                    NO,   0, FALSE)
 /*    LEVEL, SUB-LEVEL, REG, POS, SIZE, NAME,             MON SUPP, HWV, CPL3 */
 #define CPUID_FIELD_DATA_LEVEL_12                                              \
 FLAG(  12,  0, EAX,  0,  1, SGX1,                              YES, 17, FALSE) \
-FLAG(  12,  0, EAX,  1,  1, SGX2,                              NO,   0, FALSE) \
+FLAG(  12,  0, EAX,  1,  1, SGX2,                              YES, 17, FALSE) \
 FLAG(  12,  0, EBX,  0,  1, SGX_MISCSELECT_EXINFO,             YES, 17, FALSE) \
 FIELD( 12,  0, EBX,  1, 31, SGX_MISCSELECT_RSVD,               NO,   0, FALSE) \
 FIELD( 12,  0, EDX,  0,  8, MAX_ENCLAVE_SIZE_NOT64,            YES, 17, FALSE) \
