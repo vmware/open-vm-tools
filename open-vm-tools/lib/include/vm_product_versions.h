0c34a86b232a9904941f825ce8b16320cadaace1
Oliver Kurth <okurth@vmware.com>
2018-09-17 16:41:17 -0700

    Changes to common header files not directly applicable to open-vm-tools
    VGAuth Windows: fix file properties product version and file version
    
    Product name of the VGAuth binaries are set to VMware Workstation and
    the Product version set to experimental (e.x.p).
    
    Experimental version has the e.x.p build number in the Product Version
    field of each of the shipped binaries. Now that we are WHQL signing a DLL too
    for security it does make sense for the feature to use an official version.
    
    This changes the Product name and version fields from
    Product name -> VMware Workstation
    Product version -> e.x.p build-4013326
    
    to
    Product name -> VMware Guest Authentication
    Product version -> 1.0.0 build-4013326

--- open-vm-tools/lib/include/vm_product_versions.h
+++ open-vm-tools/lib/include/vm_product_versions.h
@@ -260,7 +260,7 @@
 #define VCSA_INSTALLER_VERSION "1.0.0"
 #define OVFTOOL_FILE_VERSION 4,3,0,PRODUCT_BUILD_NUMBER_NUMERIC
 #define VDM_CLIENT_VERSION "4.5.1"
-#define VGAUTH_VERSION "e.x.p"
+#define VGAUTH_VERSION "1.0.0"
 #define COMMON_AGENT_VERSION "e.x.p"
 #define VIEWY_VERSION "e.x.p"
 #define VMCFSDK_VERSION "e.x.p"
@@ -355,7 +355,9 @@
 #  define PRODUCT_VERSION_NUMBER ESX_VERSION
 #elif defined(VMX86_HORIZON_VIEW)
 #  define PRODUCT_VERSION_NUMBER VIEW_VERSION
-// VMX86_DESKTOP must be last because it is the default and is always defined.
+#elif defined(VMX86_VGAUTH)
+#  define PRODUCT_VERSION_NUMBER VGAUTH_VERSION
+ // VMX86_DESKTOP must be last because it is the default and is always defined.
 #elif defined(VMX86_DESKTOP)
 #  if defined(__APPLE__)
 #    define PRODUCT_VERSION_NUMBER FUSION_VERSION
