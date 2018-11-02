b0fcac5dd34133a6a4baa926b4a99a7a02599268
Oliver Kurth <okurth@vmware.com>
2018-11-02 15:28:17 -0700

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/backdoor_def.h
+++ open-vm-tools/lib/include/backdoor_def.h
@@ -237,6 +237,7 @@ extern "C" {
 #  define BDOOR_CMD_GMM_MAP_MEMORY            1
 #  define BDOOR_CMD_GMM_GET_PPN_AND_SZ        2 /* Devel only. */
 #  define BDOOR_CMD_GMM_ENTER                 3
+#  define BDOOR_CMD_GMM_ONESHOT_TIMER         4
 #define   BDOOR_CMD_MAX                      97
 
 
