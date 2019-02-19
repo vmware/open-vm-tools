393476a4ccab5dbf1afe1ac5ced9f71be0d25cff
Oliver Kurth <okurth@vmware.com>
2019-02-19 12:51:33 -0800

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_basic_types.h
+++ open-vm-tools/lib/include/vm_basic_types.h
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 1998-2018 VMware, Inc. All rights reserved.
+ * Copyright (C) 1998-2019 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -536,7 +536,7 @@ typedef uint128 UReg128;
 #endif
 
 #if defined(VMM) || defined(COREQUERY) || defined(EXTDECODER) ||  \
-    defined (VMKERNEL) || defined (VMKBOOT)
+    defined (VMKERNEL) || defined (VMKBOOT) || defined (ULVMM)
 typedef  Reg64  Reg;
 typedef UReg64 UReg;
 #endif
