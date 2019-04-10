306f0f7b507486795c0d726288ceb7145dcf3c00
Oliver Kurth <okurth@vmware.com>
2019-04-10 14:14:57 -0700

    Common header file change no applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_device_version.h
+++ open-vm-tools/lib/include/vm_device_version.h
@@ -54,6 +54,7 @@
  *    VMware HD Audio controller
  */
 #define PCI_VENDOR_ID_VMWARE                    0x15AD
+#define PCI_DEVICE_ID_VMWARE_SVGA3              0x0406
 #define PCI_DEVICE_ID_VMWARE_SVGA2              0x0405
 #define PCI_DEVICE_ID_VMWARE_SVGA               0x0710
 #define PCI_DEVICE_ID_VMWARE_VGA                0x0711
