5b7554bd858890184ef34519214d4abe0f402424
Oliver Kurth <okurth@vmware.com>
2019-09-09 11:23:49 -0700

    Include AppInfo related files in Tools OVT
    This changeset contains the necessary changes to include appinfo plugin
    related files in Tools OVT bundle.

---
+++ open-vm-tools/lib/include/appInfo.h
@@ -0,0 +1,41 @@
+/*********************************************************
+ * Copyright (C) 2019 VMware, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation version 2.1 and no later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *********************************************************/
+
+#ifndef _APPINFO_H_
+#define _APPINFO_H_
+
+/**
+ * @file appInfo.h
+ *
+ * Declares few constants for the appInfo plugin.
+ */
+
+/**
+ * The guest Variable name for the application information.
+ */
+#define APP_INFO_GUESTVAR_KEY         "appInfo"
+
+#define APP_INFO_VERSION_1            1
+#define APP_INFO_KEY_VERSION          "version"
+#define APP_INFO_KEY_UPDATE_COUNTER   "updateCounter"
+#define APP_INFO_KEY_PUBLISHTIME      "publishTime"
+#define APP_INFO_KEY_APPS             "applications"
+#define APP_INFO_KEY_APP_NAME         "a"
+#define APP_INFO_KEY_APP_VERSION      "v"
+
+#endif /* _APPINFO_H_ */
