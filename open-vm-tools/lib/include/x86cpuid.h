3518ebdfbe2ac695e5533e0fb9ad5887ecae53be
Oliver Kurth <okurth@vmware.com>
2020-01-06 15:46:19 -0800

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/x86cpuid.h
+++ open-vm-tools/lib/include/x86cpuid.h
@@ -545,7 +545,7 @@ FLAG(   7,  0, ECX, 14,  1, AVX512VPOPCNTDQ,                     YES,  16 ) \
 FLAG(   7,  0, ECX, 16,  1, VA57,                                NO,    0 ) \
 FIELD(  7,  0, ECX, 17,  5, MAWA,                                NO,    0 ) \
 FLAG(   7,  0, ECX, 22,  1, RDPID,                               YES,  17 ) \
-FLAG(   7,  0, ECX, 25,  1, CLDEMOTE,                            NO,    0 ) \
+FLAG(   7,  0, ECX, 25,  1, CLDEMOTE,                            YES, FUT ) \
 FLAG(   7,  0, ECX, 27,  1, MOVDIRI,                             YES, FUT ) \
 FLAG(   7,  0, ECX, 28,  1, MOVDIR64B,                           NO,    0 ) \
 FLAG(   7,  0, ECX, 29,  1, ENQCMD,                              NO,    0 ) \
