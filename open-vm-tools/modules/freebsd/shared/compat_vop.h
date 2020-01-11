4a1be44a009f3aa0867eb021f846bfca060845e1
Josh Paetzel <jpaetzel@panzura.com>
2020-01-11 10:53:20 -0600

    Fix build on FreeBSD HEAD

--- open-vm-tools/modules/freebsd/shared/compat_vop.h
+++ open-vm-tools/modules/freebsd/shared/compat_vop.h
@@ -47,7 +47,11 @@
 #if __FreeBSD_version >= 800011
 #define COMPAT_THREAD_VAR(varname, varval)
 #define COMPAT_VOP_LOCK(vop, flags, threadvar) VOP_LOCK((vop), (flags))
+#if __FreeBSD_version >= 1300074
+#define COMPAT_VOP_UNLOCK(vop, flags, threadvar) VOP_UNLOCK((vop))
+#else
 #define COMPAT_VOP_UNLOCK(vop, flags, threadvar) VOP_UNLOCK((vop), (flags))
+#endif
 #define compat_lockstatus(lock, threadvar) lockstatus((lock))
 #define compat_lockmgr(lock, flags, randompointerparam, threadval) lockmgr((lock), (flags), (randompointerparam))
 #define compat_vn_lock(vp, flags, threadval) vn_lock((vp), (flags))
--- open-vm-tools/modules/freebsd/vmblock/vnops.c
+++ open-vm-tools/modules/freebsd/vmblock/vnops.c
@@ -1262,12 +1262,15 @@ struct vop_unlock_args {
 */
 {
    struct vnode *vp = ap->a_vp;
+#if __FreeBSD_version < 1300074
    int flags = ap->a_flags;
+#endif
    COMPAT_THREAD_VAR(td, ap->a_td);
    struct VMBlockNode *nn;
    struct vnode *lvp;
    int error;
 
+#if __FreeBSD_version < 1300074
    /*
     * If caller already holds interlock, drop it.  (Per VOP_UNLOCK() API.)
     * Also strip LK_INTERLOCK from flags passed to lower layer.
@@ -1276,6 +1279,7 @@ struct vop_unlock_args {
       VI_UNLOCK(vp);
       ap->a_flags = flags &= ~LK_INTERLOCK;
    }
+#endif
    nn = VPTOVMB(vp);
    if (nn != NULL && (lvp = VMBVPTOLOWERVP(vp)) != NULL) {
       error = COMPAT_VOP_UNLOCK(lvp, flags, td);
