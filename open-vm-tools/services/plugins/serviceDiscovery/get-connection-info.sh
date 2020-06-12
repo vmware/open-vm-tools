d5517e5255770cd66eab3239c9d617147a3b0077
Oliver Kurth <okurth@vmware.com>
2020-06-11 20:43:21 -0700

    ServiceDiscovery: Replacing deprecated 'netstat' command with 'ss'
    Updating the Linux serviceDiscovery scripts to use "ss" in place of
    the deprecated "netstat" command.

--- open-vm-tools/services/plugins/serviceDiscovery/get-connection-info.sh
+++ open-vm-tools/services/plugins/serviceDiscovery/get-connection-info.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # check if necesary commands exist
-command -v netstat >/dev/null 2>&1 || { echo >&2 "netstat doesn't exist"; exit 1; }
+command -v ss >/dev/null 2>&1 || { echo >&2 "ss doesn't exist"; exit 1; }
 command -v grep >/dev/null 2>&1 || { echo >&2 "grep doesn't exist"; exit 1; }
 command -v sort >/dev/null 2>&1 || { echo >&2 "sort doesn't exist"; exit 1; }
 command -v tr >/dev/null 2>&1 || { echo >&2 "tr doesn't exist"; exit 1; }
 
 # get pids of listening processes
-space_separated_pids=$(netstat -lntup | grep -Eo "[0-9]+/" | sort -u)
+space_separated_pids=$(ss -lntup | grep -Eo "pid=[0-9]+" | sort -u)
 
 # grep pattern
 pattern=$(echo $space_separated_pids | tr ' ' '|')
 
 # get matching lines
-netstat -antup | grep -E $pattern
+ss -antup | grep -E $pattern
