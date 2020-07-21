de28bcaf8e4e85bdfcc2d67577bb2a3656f76d4f
John Wolfe <jwolfe@vmware.com>
2020-07-21 11:52:59 -0700

    Backout the previous vgauth changes for openssl 1.1.1.

--- open-vm-tools/vgauth/serviceImpl/saml-xmlsec1.c
+++ open-vm-tools/vgauth/serviceImpl/saml-xmlsec1.c
@@ -1120,7 +1120,7 @@ BuildCertChain(xmlNodePtr x509Node,
        */
       ret = xmlSecCryptoAppKeysMngrCertLoadMemory(mgr,
                                                   pemCert,
-                                                  (xmlSecSize) strlen(pemCert),
+                                                  strlen(pemCert),
                                                   xmlSecKeyDataFormatPem,
                                                   xmlSecKeyDataTypeTrusted);
       if (ret < 0) {
