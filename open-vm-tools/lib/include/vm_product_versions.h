e0152be9ef541f359bf2cc4452b838825cefd581
John Wolfe <jwolfe@vmware.com>
2020-07-21 11:53:00 -0700

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_product_versions.h
+++ open-vm-tools/lib/include/vm_product_versions.h
@@ -278,7 +278,7 @@
 #define PRECHECK_VERSION "e.x.p"
 #define VHSESDK_VERSION "1.0.0"
 #define VIEWVC_VERSION "14.0.2"
-#define WCP_VERSION "0.0.3"
+#define WCP_VERSION "0.0.4"
 #define VSTATS_VERSION "0.0.1"
 #define HDCS_VERSION "0.0.1"
 #define XVP_VERSION "1.0.0"
