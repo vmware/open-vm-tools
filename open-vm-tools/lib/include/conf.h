f078797c6b17ae91e3788da47a606313b141d0d9
John Wolfe <jwolfe@vmware.com>
2020-07-21 11:52:59 -0700

    Common source file changes not directly applicable to open-vm-tools at this time.

--- open-vm-tools/lib/include/conf.h
+++ open-vm-tools/lib/include/conf.h
@@ -495,4 +495,24 @@
  ******************************************************************************
  */
 
+/*
+ ******************************************************************************
+ * BEGIN CarbonBlack goodies.
+ */
+
+/**
+ * Define the string used for carbonblack config file group
+ */
+#define CONFGROUPNAME_CARBONBLACK "carbonblack"
+
+/**
+ * Defines user-defined polling interval in seconds.
+ */
+#define CONFNAME_CARBONBLACK_POLLINTERVAL "poll-interval"
+
+/*
+ ******************************************************************************
+ * END CarbonBlack goodies.
+ */
+
 #endif /* __CONF_H__ */
