09c15cc2d69d00799361b1ffc3b87550377b5f31
John Wolfe <jwolfe@vmware.com>
2020-08-07 12:03:26 -0700

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/uuid.h
+++ open-vm-tools/lib/include/uuid.h
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 1998-2018 VMware, Inc. All rights reserved.
+ * Copyright (C) 1998-2018, 2020 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -140,6 +140,9 @@ EFIGUID;
 
 Bool UUID_ConvertPackedToBin(EFIGUID *destID,
                              const char *text);
+
+Bool UUID_ConvertPackedToUUIDRFC4122(UUIDRFC4122 *destID,
+                                     const char *text);
 
 Bool UUID_ConvertToBin(uint8 dest_id[UUID_SIZE],
                        const char *text);
