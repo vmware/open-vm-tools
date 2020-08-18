79790f72cac52853dba8abba51e51226dad721f0
John Wolfe <jwolfe@vmware.com>
2020-08-18 07:46:26 -0700

    Update copyright dates.

--- open-vm-tools/lib/include/vm_basic_asm_arm64.h
+++ open-vm-tools/lib/include/vm_basic_asm_arm64.h
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2013-2017 VMware, Inc. All rights reserved.
+ * Copyright (C) 2013-2017, 2020 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
