a8c4c37d27fdb255bf923a964efbfaf09a235c67
Vince <git@darkain.com>
2020-10-14 20:11:15 -0700

    Adding FreeBSD aarch64 hardware register support

--- open-vm-tools/lib/include/sigPosixRegs.h
+++ open-vm-tools/lib/include/sigPosixRegs.h
@@ -189,6 +189,8 @@ extern "C" {
 #define SC_R13(uc) ((unsigned long) (uc)->uc_mcontext.mc_r13)
 #define SC_R14(uc) ((unsigned long) (uc)->uc_mcontext.mc_r14)
 #define SC_R15(uc) ((unsigned long) (uc)->uc_mcontext.mc_r15)
+#elif defined(__aarch64__)
+#define SC_X(uc,n) ((unsigned long) (uc)->uc_mcontext.mc_gpregs.gp_x[n])
 #else
 #define SC_EAX(uc) ((unsigned long) (uc)->uc_mcontext.mc_eax)
 #define SC_EBX(uc) ((unsigned long) (uc)->uc_mcontext.mc_ebx)
