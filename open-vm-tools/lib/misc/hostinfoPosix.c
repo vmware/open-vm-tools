ebad65a09795690801d2e047d1b6f7bc1f0d9fd6
Vincent Milum Jr <git@darkain.com>
2020-10-14 21:04:40 -0700

    Add support for "arm64" string
    Some OSes use "arm64" others use "aarch64"

--- open-vm-tools/lib/misc/hostinfoPosix.c
+++ open-vm-tools/lib/misc/hostinfoPosix.c
@@ -140,11 +140,13 @@
 #define SYSTEM_BITNESS_64_SUN "amd64"
 #define SYSTEM_BITNESS_64_LINUX "x86_64"
 #define SYSTEM_BITNESS_64_ARM_LINUX "aarch64"
+#define SYSTEM_BITNESS_64_ARM_FREEBSD "arm64"
 #define SYSTEM_BITNESS_MAXLEN \
    MAX(sizeof SYSTEM_BITNESS_32, \
    MAX(sizeof SYSTEM_BITNESS_64_SUN, \
    MAX(sizeof SYSTEM_BITNESS_64_LINUX, \
-       sizeof SYSTEM_BITNESS_64_ARM_LINUX)))
+   MAX(sizeof SYSTEM_BITNESS_64_ARM_LINUX, \
+       sizeof SYSTEM_BITNESS_64_ARM_FREEBSD))))
 
 struct hostinfoOSVersion {
    int   hostinfoOSVersion[4];
@@ -479,7 +481,8 @@ Hostinfo_GetSystemBitness(void)
       return 32;
    } else if (strcmp(buf, SYSTEM_BITNESS_64_SUN) == 0 ||
               strcmp(buf, SYSTEM_BITNESS_64_LINUX) == 0 ||
-              strcmp(buf, SYSTEM_BITNESS_64_ARM_LINUX) == 0) {
+              strcmp(buf, SYSTEM_BITNESS_64_ARM_LINUX) == 0 ||
+              strcmp(buf, SYSTEM_BITNESS_64_ARM_FREEBSD) == 0) {
       return 64;
    }
 
