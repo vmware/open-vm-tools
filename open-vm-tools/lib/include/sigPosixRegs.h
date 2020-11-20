24cb1ecb455d9d1e348a2eb2bc7ced692e9f4f6d
John Wolfe <jwolfe@vmware.com>
2020-11-20 08:37:30 -0800

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/sigPosixRegs.h
+++ open-vm-tools/lib/include/sigPosixRegs.h
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2009-2017 VMware, Inc. All rights reserved.
+ * Copyright (C) 2009-2017,2020 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -39,7 +39,7 @@ extern "C" {
 #if __linux__         // We need the REG_foo offsets in the gregset_t;
 #  define _GNU_SOURCE // _GNU_SOURCE maps to __USE_GNU
 
-/* And, the REG_foo definitions conflict with our own in x86.h */
+/* And, the REG_foo definitions conflict with our own in x86/regname_arch.h */
 #  if defined(__x86_64__)
 #    define REG_RAX GNU_REG_RAX
 #    define REG_RBX GNU_REG_RBX
