ff5eb5f448c78448b96f9c3db957d19f0288a9b6
John Wolfe <jwolfe@vmware.com>
2021-02-04 09:57:03 -0800

    resolution: Fix kms autodetection
    Currently, the elf binary of the xorg driver is loaded to check for a
    string that was put in the .modinfo section in the driver.  Unfortunately
    there are two problems with this approach:
    
      1) Distros now ship without xorg, so the xorg .so doesn't exist and
         there's nothing to check.
      2) Distros (e.g. Fedora) do heavy optimizations and remove the .modinfo
         section from the .so, so the string cannot be found even though the
         driver exists.
    
    To fix both, stop depending on being able to parse the elf binary of the
    xorg driver.  Instead, let the plugin check for the existence of the drm
    driver with a sufficiently high version, and if it exists, use kms.
    This removes the dependency on X for kms.  Also increase the version of
    vmwgfx required to ensure atomic mode-setting plus relevant bug fixes (in
    the kernel since 2017) are available.

--- open-vm-tools/services/plugins/resolutionSet/resolutionCommon.h
+++ open-vm-tools/services/plugins/resolutionSet/resolutionCommon.h
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2016 VMware, Inc. All rights reserved.
+ * Copyright (C) 2016-2021 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -41,13 +41,5 @@ static inline int resolutionCheckForKMS(ToolsAppCtx *ctx)
 static inline void resolutionDRMClose(int fd) {}
 
 #endif /* !ENABLE_RESOLUTIONKMS */
-
-int
-resolutionXorgDriverVersion(int numPaths,
-			    const char *paths[],
-			    const char versionString[],
-			    int *major,
-			    int *minor,
-			    int *level);
 
 #endif /*  _RESOLUTION_COMMON_H_ */
