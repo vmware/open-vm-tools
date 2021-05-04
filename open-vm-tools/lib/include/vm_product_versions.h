b2078e875dfaa3257cb8a709a574b8a3cb8e8069
John Wolfe <jwolfe@vmware.com>
2021-05-03 19:39:40 -0700

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_product_versions.h
+++ open-vm-tools/lib/include/vm_product_versions.h
@@ -45,7 +45,7 @@
    /* this should be kept in sync with the corresponding vpx branch. */
    #define PRODUCT_VERSION    7,0,3,PRODUCT_BUILD_NUMBER_NUMERIC
 #elif defined(VMX86_HORIZON_VIEW)
-   #define PRODUCT_VERSION    8,3,0,PRODUCT_BUILD_NUMBER_NUMERIC
+   #define PRODUCT_VERSION    8,4,0,PRODUCT_BUILD_NUMBER_NUMERIC
 // VMX86_DESKTOP must be last because it is the default and is always defined.
 #elif defined(VMX86_DESKTOP)
    // WORKSTATION_VERSION_NUMBER below has to match this
@@ -292,24 +292,24 @@
  * SCons parsing code requires that each line have a version string, so we
  * can't just do something like #define RDESDK_VERSION VIEW_VERSION"
  */
-#define VIEW_CLIENT_VERSION "8.3.0"
+#define VIEW_CLIENT_VERSION "8.4.0"
 #define VIEW_CLIENT_VERSION_NUMBER VIEW_CLIENT_VERSION
-#define VIEW_VERSION "8.3.0"
-#define RDE_RFT_ALL_VERSION "8.3.0"
-#define RDE_RFT_REL_VERSION "8.3.0"
-#define RDESDKREL_VERSION "8.3.0"
-#define MKSVCHANDEV_VERSION "15.3.0"
-#define TSMMRDEV_VERSION "8.3.0"
-#define RDF_VERSION "8.3.0"
-#define HORIZON_DAAS_AGENT_VERSION "21.2.0"
-#define HORIZON_USB_AGENT_VERSION "11.3.0"
-#define MFW_VERSION "16.3.0"
+#define VIEW_VERSION "8.4.0"
+#define RDE_RFT_ALL_VERSION "8.4.0"
+#define RDE_RFT_REL_VERSION "8.4.0"
+#define RDESDKREL_VERSION "8.4.0"
+#define MKSVCHANDEV_VERSION "15.4.0"
+#define TSMMRDEV_VERSION "8.4.0"
+#define RDF_VERSION "8.4.0"
+#define HORIZON_DAAS_AGENT_VERSION "21.3.0"
+#define HORIZON_USB_AGENT_VERSION "11.4.0"
+#define MFW_VERSION "16.4.0"
 /*
  * This is the Horizon Marketing version which is shared between
  * Horizon Client, Agent and Server. It is set in the format of
  * YYMM of the targeted release date.
  */
-#define HORIZON_YYMM_VERSION "2106"
+#define HORIZON_YYMM_VERSION "2109"
 
 #ifndef MAKESTR
 #define MAKESTR(x) #x
