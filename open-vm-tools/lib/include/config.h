26e7c2f3b5198621c8234c4890431dc16698bcf3
John Wolfe <jwolfe@vmware.com>
2021-07-27 10:37:24 -0700

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/config.h
+++ open-vm-tools/lib/include/config.h
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 1998-2018 VMware, Inc. All rights reserved.
+ * Copyright (C) 1998-2018,2021 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -41,6 +41,8 @@ struct KeySafeUserRing;
 
 void Config_SetAny(const char *value,
                    const char *fmt, ...) PRINTF_DECL(2, 3);
+void Config_SetAnySecure(const char *value,
+                         const char *fmt, ...) PRINTF_DECL(2, 3);
 void Config_SetString(const char *value,
                       const char *fmt, ...) PRINTF_DECL(2, 3);
 void Config_SetStringPlain(const char *value,
