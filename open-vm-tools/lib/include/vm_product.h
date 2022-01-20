ddd97d710b1b49a2d8b3679ac07c70feacb2aeb0
John Wolfe <jwolfe@vmware.com>
2022-01-20 11:40:15 -0800

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_product.h
+++ open-vm-tools/lib/include/vm_product.h
@@ -196,6 +196,7 @@
 
 #if !(   defined(VMX86_ESXIO)    \
       || defined(VMX86_SERVER)   \
+      || defined(VMKBOOT)        \
       || defined(VMX86_DESKTOP)  \
       || defined(VMX86_HORIZON_VIEW)     \
       || defined(VMX86_VPX)      \
@@ -226,6 +227,8 @@
 #if defined(VMX86_ESXIO)
 # define PRODUCT_SHORT_NAME PRODUCT_ESXIO_NAME
 #elif defined(VMX86_SERVER)
+# define PRODUCT_SHORT_NAME PRODUCT_ESXI_NAME
+#elif defined(VMKBOOT)
 # define PRODUCT_SHORT_NAME PRODUCT_ESXI_NAME
 #elif defined(VMX86_VMRC) /* check VMX86_VMRC before VMX86_DESKTOP */
 # define PRODUCT_SHORT_NAME PRODUCT_VMRC_NAME
