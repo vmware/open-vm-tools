f48b8797c14d43b3786fddae8bd38b890e48cd9d
John Wolfe <jwolfe@vmware.com>
2022-01-20 11:40:15 -0800

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_legal.h
+++ open-vm-tools/lib/include/vm_legal.h
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2006-2020 VMware, Inc. All rights reserved.
+ * Copyright (C) 2006-2022 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -37,7 +37,7 @@
  * Updating them therefore may require updating translations and vmsg
  * auditing files in bora/messages/ to avoid breaking the build.
  */
-#define COPYRIGHT_YEARS  "1998-2021" /* See the note above when changing. */
+#define COPYRIGHT_YEARS  "1998-2022" /* See the note above when changing. */
 #define COPYRIGHT_STRING "Copyright (C) " COPYRIGHT_YEARS " " COMPANY_NAME
 #define RIGHT_RESERVED   "All rights reserved."
 
