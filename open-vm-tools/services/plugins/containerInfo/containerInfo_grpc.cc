2290690693f0590bb5ffbd592ebd4b8062d57513
John Wolfe <jwolfe@vmware.com>
2022-02-07 09:40:00 -0800

    AppVisibility: Skip terminated/stopped containers.
    Added code to skip terminated/stopped containers.  This is done
    by using TaskStub to retrieve the pid for the running container.
    If the pid cannot be retrieved, then that container is skipped.
    
    The previously suppressed "default" namespace has been restored.
    
    Fixed another minor issue with calculating the number of containers
    that were added.
    
    Note: This changeset introduces a dependency on Tasks.proto, and
    Tasks.proto depends on a few other proto files like mount, metrics,
    descriptor and task.  Modified the makefile to generate the necessary
    C++ files from the proto files.
    
    Additional notes:
     - .proto -> generates .pb.h, .pb.c and .grpc.pb.cc files.
     - For compiling and building the library (.so file), the .grpc.pb.{h,cc}
       and .pb.{h,cc} files are needed.
     - The protoc compiler generates .pb.h and .pb.c files when the --cpp_out
       option is specified.
     - The protoc compiler generates .grpc.pb.* when the --grpc_out option is
       specified.

--- open-vm-tools/services/plugins/containerInfo/containerInfo_grpc.cc
+++ open-vm-tools/services/plugins/containerInfo/containerInfo_grpc.cc
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2021 VMware, Inc. All rights reserved.
+ * Copyright (C) 2021-2022 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -27,11 +27,13 @@
 
 #include "containerInfoInt.h"
 #include "containers.grpc.pb.h"
-#include "containers.pb.h"
+#include "tasks.grpc.pb.h"
 #include <grpc++/grpc++.h>
 #include <stdio.h>
 
 using namespace containerd::services::containers::v1;
+using namespace containerd::services::tasks::v1;
+using namespace containerd::v1::types;
 using namespace google::protobuf;
 
 
@@ -62,8 +64,10 @@ ContainerInfo_GetContainerList(const char *ns,                   // IN
    GSList *containerList = NULL;
    std::shared_ptr<grpc::ChannelInterface> channel;
    std::unique_ptr<Containers::Stub> containerStub;
+   std::unique_ptr<Tasks::Stub> taskStub;
    grpc::Status status;
    int i;
+   int containersAdded;
    const ListContainersRequest req;
    std::unique_ptr<ListContainersResponse> res;
    grpc::ClientContext containerContext;
@@ -94,6 +98,12 @@ ContainerInfo_GetContainerList(const char *ns,                   // IN
       goto exit;
    }
 
+   taskStub = Tasks::NewStub(channel);
+   if (taskStub == nullptr) {
+      g_warning("%s: Failed to create taskStub\n", __FUNCTION__);
+      goto exit;
+   }
+
    res = std::make_unique<ListContainersResponse>();
    status = containerStub->List(&containerContext, req, res.get());
 
@@ -106,18 +116,39 @@ ContainerInfo_GetContainerList(const char *ns,                   // IN
    g_debug("%s: Namespace: '%s', number of containers found: %d", __FUNCTION__,
            ns, res->containers_size());
 
-   for (i = 0; i < res->containers_size() && i < maxContainers; i++) {
+   for (i = 0, containersAdded = 0;
+        i < res->containers_size() && containersAdded < maxContainers; i++) {
       Container curContainer = res->containers(i);
       std::string id = curContainer.id();
       std::string image = curContainer.image();
-      ContainerInfo *info = (ContainerInfo *)g_malloc(sizeof(*info));
+      ContainerInfo *info;
+      GetRequest taskReq;
+      std::unique_ptr<GetResponse> taskRes;
+      grpc::Status taskStatus;
+      grpc::ClientContext taskContext;
 
+      taskContext.AddMetadata(namespaceKey, ns);
+
+      /*
+       * Get pid for container using taskStub.
+       */
+      taskRes = std::make_unique<GetResponse>();
+      taskReq.set_container_id(id);
+      taskStatus = taskStub->Get(&taskContext, taskReq, taskRes.get());
+      if (!taskStatus.ok()) {
+         g_debug("%s: Task get service failed: %s. skipping container: %s\n",
+                 __FUNCTION__, taskStatus.error_message().c_str(), id.c_str());
+         continue;
+      }
+
+      info = (ContainerInfo *)g_malloc(sizeof(*info));
       info->id = g_strdup(id.c_str());
       info->image = g_strdup(image.c_str());
 
       g_debug("%s: Found container id: %s and image: %s\n", __FUNCTION__,
               info->id, info->image);
       containerList = g_slist_prepend(containerList, info);
+      containersAdded++;
    }
 
 exit:
