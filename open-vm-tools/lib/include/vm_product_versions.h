53f331ccbbbc57eeaf4dfc51c94ec6f3e9a31a65
John Wolfe <jwolfe@vmware.com>
2022-09-08 14:51:39 -0700

    Common header file change not applicable to open-vm-tools.

--- open-vm-tools/lib/include/vm_product_versions.h
+++ open-vm-tools/lib/include/vm_product_versions.h
@@ -546,7 +546,7 @@
 #  define PRODUCT_VERSION_STRING_FOR_LICENSE PRODUCT_LICENSE_VERSION
 #endif
 #define PRODUCT_ESX_LICENSE_VERSION "8.0"
-#define PRODUCT_ESX_LICENSE_FILE_VERSION "8.0.1.1"
+#define PRODUCT_ESX_LICENSE_FILE_VERSION "8.0.1.2"
 
 /*
  * The configuration file version string should be changed
