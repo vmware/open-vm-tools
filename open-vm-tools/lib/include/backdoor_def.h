e49ffc8d24257a4c1568e3a36d1f001a16b9d4cc
John Wolfe <jwolfe@vmware.com>
2022-09-21 10:53:28 -0700

    Change to common header file not applicable to open-vm-tools.

--- open-vm-tools/lib/include/backdoor_def.h
+++ open-vm-tools/lib/include/backdoor_def.h
@@ -171,6 +171,7 @@ extern "C" {
 //#define BDOOR_CMD_FAS_GET_NVDIMM_FMT_CODE  11 /* Not in use. Never shipped. */
 #  define BDOOR_CMD_FAS_SRP_ENABLED          12
 #  define BDOOR_CMD_FAS_EXIT_BOOT_SERVICES   13
+#  define BDOOR_CMD_FAS_GET_API_ENABLES      14
 #define   BDOOR_CMD_SENDPSHAREHINTS          66 /* Not in use. Deprecated. */
 #define   BDOOR_CMD_ENABLE_USB_MOUSE         67
 #define   BDOOR_CMD_GET_VCPU_INFO            68
@@ -307,6 +308,8 @@ extern "C" {
 #define BDOOR_SECUREBOOT_STATUS_DISABLED  0xFFFFFFFFUL
 #define BDOOR_SECUREBOOT_STATUS_APPROVED  1
 #define BDOOR_SECUREBOOT_STATUS_DENIED    2
+
+#define BDOOR_FAS_API_ENABLE_FDT          0x00000001UL
 
 /* High-bandwidth backdoor port. */
 
