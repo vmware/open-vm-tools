7a94ee4524da9c794a68d6b2e7106d719209d93e
Katy Feng <fkaty@vmware.com>
2022-11-29 12:11:42 -0800

    Change to common header file not applicable to open-vm-tools.

--- open-vm-tools/lib/include/log.h
+++ open-vm-tools/lib/include/log.h
@@ -443,14 +443,20 @@ Log_SetOutputLevel(LogOutput *output,
                    int32 level);
 
 /*
- * Structure contains all the pointers to where value can be updated
+ * Structure contains all the pointers to where value can be updated.
  * Making VmxStats as a struct has its own advantage, such as updating
  * 'droppedChars' from the struct instead within LogFile.
  */
+
+struct VmxStatMinMax64;
+
 typedef struct {
-   uint64 *numTimesDrop; // total time char dropped
-   uint64 *droppedChars; // Number of drop char
-   uint64 *bytesLogged;  // Total logged
+   uint64          *logMsgsDropped;    // Number of dropped messages
+   uint64          *logBytesDropped;   // Number of drop bytes
+   uint64          *logBytesLogged;    // Bytes logged
+
+   struct VmxStatMinMax64 *logWriteMinMaxTime; // Min/max write time in US
+   uint64          *logWriteAvgTime;   // Average time to write in US
 } VmxStatsInfo;
 
 Bool
@@ -599,9 +605,6 @@ Log_SkipLocking(Bool skipLocking);
 
 void
 Log_DisableThrottling(void);
-
-void
-Log_DisableVmxStats(void);
 
 uint32
 Log_MaxLineLength(void);
