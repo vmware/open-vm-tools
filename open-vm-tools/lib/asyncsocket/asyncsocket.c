7173a629c81bd6acf45cb07dbb169fda5ab73adf
Katy Feng <fkaty@vmware.com>
2023-02-23 11:27:13 -0800

    Change to common source file not applicable to open-vm-tools.

--- open-vm-tools/lib/asyncsocket/asyncsocket.c
+++ open-vm-tools/lib/asyncsocket/asyncsocket.c
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2003-2022 VMware, Inc. All rights reserved.
+ * Copyright (C) 2003-2023 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -6159,7 +6159,7 @@ AsyncTCPSocketConnectSSL(AsyncSocket *base,           // IN
    ASSERT(asock);
 
    if (sslContext == NULL) {
-      sslContext = SSL_DefaultContext();
+      sslContext = SSL_DefaultClientContext();
    }
 
    return SSL_ConnectAndVerifyWithContext(asock->sslSock, verifyParam,
