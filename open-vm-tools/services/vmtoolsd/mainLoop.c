641e13fc05118ff8215f9461df424e277cba73dc
Katy Feng <fkaty@vmware.com>
2023-08-25 11:58:47 -0700

    Changes to common source files not applicable to open-vm-tools.

--- open-vm-tools/services/vmtoolsd/mainLoop.c
+++ open-vm-tools/services/vmtoolsd/mainLoop.c
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (c) 2008-2022 VMware, Inc. All rights reserved.
+ * Copyright (c) 2008-2023 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -1201,7 +1201,12 @@ ToolsCore_Setup(ToolsServiceState *state)
 #else
    state->ctx.mainLoop = g_main_loop_new(gctx, FALSE);
 #endif
+   /*
+    * TODO: Build vmcheck library
+    */
+#ifndef _ARM64_
    state->ctx.isVMware = VmCheck_IsVirtualWorld();
+#endif
    g_main_context_unref(gctx);
 
    g_type_init();
