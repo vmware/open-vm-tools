6ab97407104bd86f63ef4ab30462c2234b72e47e
Katy Feng <fkaty@vmware.com>
2023-10-05 10:35:27 -0700

    Change to common source file not applicable to open-vm-tools.

--- open-vm-tools/lib/asyncsocket/asyncSocketInterface.c
+++ open-vm-tools/lib/asyncsocket/asyncSocketInterface.c
@@ -1,5 +1,5 @@
 /*********************************************************
- * Copyright (C) 2016-2022 VMware, Inc. All rights reserved.
+ * Copyright (C) 2016-2023 VMware, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -383,7 +383,7 @@ AsyncSocket_UseNodelay(AsyncSocket *asyncSocket,  // IN/OUT
 {
    const int noDelayNative = noDelay ? 1 : 0;
    return AsyncSocket_SetOption(asyncSocket,
-                                IPPROTO_TCP, TCP_NODELAY,
+                                (AsyncSocketOpts_Layer)IPPROTO_TCP, TCP_NODELAY,
                                 &noDelayNative, sizeof noDelayNative);
 }
 
@@ -443,17 +443,17 @@ AsyncSocket_SetTCPTimeouts(AsyncSocket *asyncSocket,  // IN/OUT
 
       ret = VT(asyncSocket)->setOption
                (asyncSocket,
-                IPPROTO_TCP, TCP_KEEPIDLE,
+                (AsyncSocketOpts_Layer)IPPROTO_TCP, TCP_KEEPIDLE,
                 &keepIdleSec, sizeof keepIdleSec);
       if (ret == ASOCKERR_SUCCESS) {
          ret = VT(asyncSocket)->setOption
                   (asyncSocket,
-                   IPPROTO_TCP, TCP_KEEPINTVL,
+                   (AsyncSocketOpts_Layer)IPPROTO_TCP, TCP_KEEPINTVL,
                    &keepIntvlSec, sizeof keepIntvlSec);
          if (ret == ASOCKERR_SUCCESS) {
             ret = VT(asyncSocket)->setOption
                      (asyncSocket,
-                      IPPROTO_TCP, TCP_KEEPCNT,
+                      (AsyncSocketOpts_Layer)IPPROTO_TCP, TCP_KEEPCNT,
                       &keepCnt, sizeof keepCnt);
          }
       }
