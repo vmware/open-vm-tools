3e68aa8735e860e7dbe9ba125c9675ed29e2756a
Kruti Pendharkar <kp025370@broadcom.com>
2025-10-16 21:25:49 -0700

    Change to common header file not applicable to open-vm-tools.

--- open-vm-tools/lib/include/guest_os_tables.h
+++ open-vm-tools/lib/include/guest_os_tables.h
@@ -146,6 +146,7 @@ extern "C" {
    GOT(GUEST_OS_DARWIN22_64)         /* Mac OS 13 */                       \
    GOT(GUEST_OS_DARWIN23_64)         /* Mac OS 14 */                       \
    GOT(GUEST_OS_DARWIN24_64)         /* Mac OS 15 */                       \
+   GOT(GUEST_OS_DARWIN25_64)         /* Mac OS 26 */                       \
    GOT(GUEST_OS_OPENSERVER_5_AND_6)                                        \
    GOT(GUEST_OS_UNIXWARE7)                                                 \
    GOT(GUEST_OS_NETWARE4)                                                  \
